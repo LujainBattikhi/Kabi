d51f177802e2ca4879566820ae4bc009b6ccb85b
lujainb <lujain@sitech.me>
2024-12-08 17:53:06 +0300

    Specify the broker for the celery

--- Kabi/celery.py
+++ Kabi/celery.py
@@ -6,9 +6,11 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
+from django.conf import settings
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Kabi.settings')
 
-app = Celery('Kabi')
+app = Celery('Kabi',broker=settings.CELERY_BROKER_URL)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
