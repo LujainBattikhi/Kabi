944fc4acc3601eb9754057a7ac8445b194916ed0
lujainb <lujain@sitech.me>
2024-12-08 17:56:06 +0300

    Handle Bulk Update and Bulk Create jobs

--- Kabi/apps/jobs/tasks.py
+++ Kabi/apps/jobs/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+from django.db import transaction
 
 from Kabi.apps.jobs.scraper import scrape_jobs
 
@@ -9,4 +10,45 @@ def health_checker():
 
 @shared_task
 def load_jobs():
-    scrape_jobs()
+    scarped_jobs=scrape_jobs()
+    handle_jobs_bulk_update_create(scarped_jobs)
+
+def handle_jobs_bulk_update_create(scarped_jobs):
+    from Kabi.apps.jobs.models import JobPosting
+
+    existing = JobPosting.objects.values(
+        'id', 'title', 'company_name', 'location'
+    )
+    existing_dict = {
+        (item['title'], item['company_name'], item['location']): item['id']
+        for item in existing
+    }
+
+    new_jobs = []
+    updated_jobs = []
+
+    for job_data in scarped_jobs:
+        key = (job_data['title'], job_data['company_name'], job_data['location'])
+
+        if key in existing_dict:
+            updated_jobs.append(
+                JobPosting(
+                    id=existing_dict[key],
+                    title=job_data['title'],
+                    company_name=job_data['company_name'],
+                    location=job_data['location'],
+                    salary=job_data['salary'],
+                    description=job_data['description']
+                )
+            )
+        else:
+            new_jobs.append(JobPosting(**job_data))
+
+    with transaction.atomic():
+        if new_jobs:
+            JobPosting.objects.bulk_create(new_jobs)
+            print(f"Created {len(new_jobs)} new jobs.")
+
+        if updated_jobs:
+            JobPosting.objects.bulk_update(updated_jobs, ['salary', 'description'])
+            print(f"Updated {len(updated_jobs)} existing jobs.")
